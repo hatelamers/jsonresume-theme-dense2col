0845c87f813e8d0d89ce24c78d72ee010a88aa91
hatelamers <97636078+hatelamers@users.noreply.github.com>
2023-12-09 22:49:32 +0100

    added support for intl. postal addresses; implemented mailto and tel links (with bot protection)

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const Handlebars = require("handlebars");
 const moment = require('moment');
 const twemoji = require('twemoji');
 
-const langMap = require(__dirname + "/lang-map.json");
+const langMap = require(path.join(__dirname, "lang-map.json"));
+const addrFormats = require(path.join(__dirname, "addr-format.json"));
 
 var hmrUnharden = function(val) { return val; };
 
 function render(resume) {
 	if ('function' == typeof resume.harden) {
 		hmrUnharden = function(val) {
-			return val ? val.replace(/^@@@@~/, '').replace(/~@@@@$/, '') : val;
+			return 'string' == typeof(val) && val ? val.replace(/^@@@@~/, '').replace(/~@@@@$/, '') : val;
 		};
 	}
 	let lang = 'en';
@@ -30,8 +31,8 @@ function render(resume) {
 			console.warn(e);
 		}
 	}
-	const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	const tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	const css = fs.readFileSync(path.join(__dirname, "style.css"), "utf-8");
+	const tpl = fs.readFileSync(path.join(__dirname, "resume.hbs"), "utf-8");
 
 	const partialsDir = path.join(__dirname, 'partials');
 	const filenames = fs.readdirSync(partialsDir);
@@ -46,13 +47,16 @@ function render(resume) {
 	  Handlebars.registerPartial(name, template);
 	});
 
-  Handlebars.registerHelper('icon-network', iconNetwork);
-  Handlebars.registerHelper('icon-reg', iconDirectRegular);
-  Handlebars.registerHelper('icon-solid', iconDirectSolid);
-  Handlebars.registerHelper('flag', langFlag);
-  Handlebars.registerHelper('year-month', dateMonthFormat);
-  Handlebars.registerHelper('year-month-day', dateDayFormat);
-  Handlebars.registerHelper('label-lookup', labelHtml);
+	Handlebars.registerHelper('icon-network', iconNetwork);
+	Handlebars.registerHelper('icon-reg', iconDirectRegular);
+	Handlebars.registerHelper('icon-solid', iconDirectSolid);
+	Handlebars.registerHelper('flag', langFlag);
+	Handlebars.registerHelper('year-month', dateMonthFormat);
+	Handlebars.registerHelper('year-month-day', dateDayFormat);
+	Handlebars.registerHelper('label-lookup', labelHtml);
+	Handlebars.registerHelper('address-intl', addressFormat);
+	Handlebars.registerHelper('email-link', emailLink);
+	Handlebars.registerHelper('tel-link', telLink);
 
 	return Handlebars.compile(tpl)({
 		css: css,
@@ -105,6 +109,29 @@ function iconNetwork(iconName) {
   }
 }
 
+function addressFormat(location, options) {
+	if (!location) return '';
+
+	try {
+		const format = addrFormats[options.data.root.lang || 'en'] || addrFormats.en;
+		const result = format.reduce((accumulator, group) => {
+			let val = '';
+			group.forEach((key) => {
+				if (location[key]) {
+					if (val.length) val += ' ';
+					val += `<span class="${key}">${Handlebars.escapeExpression(hmrUnharden(location[key]))}</span>`;
+				}	
+			});
+			if (accumulator.length && val.length) val = '<span class="sep"></span>' + val;
+			return accumulator + val;	
+		}, '');
+		return new Handlebars.SafeString(result);	
+	} catch (e) {
+		console.warn(e);
+		return `----${e}----`;
+	}
+}
+
 function iconDirectRegular(key) {
 	return fontAwesomeHtml(`fa-regular fa-${key}`);
 }
@@ -136,6 +163,26 @@ function labelHtml(label, options) {
 		console.error(e);
 	}
 	return label;
+}
+
+function emailLink(addr) {
+	addr = hmrUnharden(addr);
+	let result = `<a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;${obfuscateURI(addr)}">${Handlebars.escapeExpression(addr).split('@').join(`<em class="junk">${Handlebars.escapeExpression((Math.random() + 1).toString(36).substring(2))}</em>@<em class="junk">${Handlebars.escapeExpression((Math.random() + 1).toString(36).substring(2))}</em>`)}</a>`;
+	return new Handlebars.SafeString(result);
+}
+
+function telLink(num) {
+	num = hmrUnharden(num);
+	let result = `<a href="&#116;&#101;&#108;&#58;${obfuscateURI(num)}">${Handlebars.escapeExpression(num).split('').reduce((acc, val) => `${acc}<em class="junk">${Handlebars.escapeExpression((Math.random() + 1).toString(36).substring(8))}</em>${val}`)}</a>`;
+	return new Handlebars.SafeString(result);
+}
+
+function obfuscateURI(str) {
+	let result = "";
+	for (let i = 0; i < str.length; i++) {
+		result += "%" + str.codePointAt(i).toString(16).toUpperCase();
+	}
+	return result;  
 }
 
 module.exports = {
